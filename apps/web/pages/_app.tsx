83beb602d37224f48cb80056fe38b6b34c4404da
Myles Berueda <myles@themapletree.io>
2022-05-29 19:27:00 -0700

    adds next-auth

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { AppProps } from 'next/app';
+import { AppProps } from "next/app";
 import { MENU_ITEMS } from "../config";
+import { SessionProvider } from "next-auth/react";
 
 import { Layout } from "ui";
 
-// @todo Hacky for now. Layout children type is wrong. The args for this 
-//       component is of type AppProps. 
-export default function App({ Component, pageProps }: any) {
+// @todo Hacky for now. Layout children type is wrong. The args for this
+//       component is of type AppProps.
+export default function App({ Component, pageProps, session }: any) {
   return (
-    <Layout menuItems={MENU_ITEMS} sx={{ background: "var(--pewter-blue)" }}>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout menuItems={MENU_ITEMS} sx={{ background: "var(--pewter-blue)" }}>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
