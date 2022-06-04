2cd87518ca66d56be3c00a24d49161cfbab6a6e9
MylesWritesCode <myles@themapletree.io>
2022-06-03 16:39:15 -1000

    adds more styles to login

--- apps/web/pages/auth/login.tsx
+++ apps/web/pages/auth/login.tsx
@@ -10,6 +10,8 @@ import NextImage from "next/image";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
+import { Card } from "ui/components/Card";
+
 import styles from "./auth.module.scss";
 
 function Login({
@@ -19,8 +21,6 @@ function Login({
   const { data: session } = useSession();
   const router = useRouter();
 
-  console.log(providers);
-
   useEffect(() => {
     if (session) {
       router.push("/");
@@ -28,57 +28,70 @@ function Login({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session]);
 
+  // "linear-gradient(45deg, rgb(64, 201, 255) 0%, rgb(232, 28, 255) 100%);",
   return (
-    <section className="flex flex-col justify-center items-center h-full">
-      <div className="flex flex-col justify-center items-center w-72 h-full">
-        <NextImage
-          src="/logo.png"
-          alt="logo"
-          height="100%"
-          width="300px"
-          objectFit="contain"
-        />
-        <form
+    <section className="flex h-full flex-col items-center justify-center">
+      <div className="flex h-full w-full flex-col items-center justify-center z-10">
+        <Card
           className={[
-            "flex flex-col justify-center items-center w-full font-bold",
-            styles.auth__form,
+            "align-center relative flex flex-col justify-center p-12 border-1",
+            // "before:bg-gradient-to-tr before:from-[#40c9ff] before:to-[#e81cff] before:blur-none",
+            "before:absolute before:z-[-1] before:h-full before:w-full",
+            "before:overflow-hidden before:top-0 before:left-0",
           ].join(" ")}
-          method="post"
-          action="/api/auth/callback/credentials"
         >
-          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <label>
-            <span>Username</span>
-            <input name="username" type="text" placeholder="Username" />
-          </label>
-          <label>
-            <span>Password</span>
-            <input name="password" type="password" placeholder="Password" />
-          </label>
-          <button type="submit" className="w-72 bg-primary-light">
-            Sign in
-          </button>
-        </form>
-        <div className="flex justify-evenly items-center m-4 w-full">
-          <div className="mx-2 bg-primary-dark w-16 h-[2px]"></div>
-          <span>or sign in with</span>
-          <div className="mx-2 bg-primary-dark w-16 h-[2px]"></div>
-        </div>
-        {providers &&
-          Object.values(providers).map((provider, i) => {
-            if (provider.id !== "email") {
-              return (
-                <div key={provider.name}>
-                  <button
-                    className="w-72 p-2 font-semibold"
-                    onClick={() => signIn(provider.id)}
-                  >
-                    {provider.name}
-                  </button>
-                </div>
-              );
-            }
-          })}
+          <NextImage
+            src="/logo.png"
+            alt="logo"
+            height="100%"
+            width="300px"
+            objectFit="contain"
+          />
+          <form
+            className={[
+              "flex w-full flex-col items-center justify-center font-bold",
+              styles.auth__form,
+            ].join(" ")}
+            method="post"
+            action="/api/auth/callback/credentials"
+          >
+            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+            <label>
+              <span>Username</span>
+              <input name="username" type="text" placeholder="Username" />
+            </label>
+            <label>
+              <span>Password</span>
+              <input name="password" type="password" placeholder="Password" />
+            </label>
+            <button type="submit" className="bg-primary-light w-72">
+              Sign in
+            </button>
+          </form>
+          {providers && Object.keys(providers).length > 1 && (
+            <div className="m-4 flex w-full items-center justify-evenly">
+              <div className="bg-primary-dark mx-2 h-[2px] w-16"></div>
+              <span>or sign in with</span>
+              <div className="bg-primary-dark mx-2 h-[2px] w-16"></div>
+            </div>
+          )}
+          {providers &&
+            Object.keys(providers).length > 1 &&
+            Object.values(providers).map((provider, i) => {
+              if (provider.id !== "email") {
+                return (
+                  <div key={provider.name}>
+                    <button
+                      className="w-72 p-2 font-semibold"
+                      onClick={() => signIn(provider.id)}
+                    >
+                      {provider.name}
+                    </button>
+                  </div>
+                );
+              }
+            })}
+        </Card>
       </div>
     </section>
   );
