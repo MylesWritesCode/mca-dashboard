ba10058777b59918203447c954f0203aacea756e
Myles Berueda <myles@themapletree.io>
2022-06-05 17:43:31 -0700

    updates next-auth schema

--- apps/web/pages/api/auth/[...nextauth].tsx
+++ apps/web/pages/api/auth/[...nextauth].tsx
@@ -1,8 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import * as argon2 from "argon2";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
 
 interface User {
   id: string;
@@ -26,24 +26,20 @@ export default NextAuth({
         },
       },
       async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-        const res = await fetch("/your/endpoint", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
+        console.log(credentials);
 
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user;
+        const user = await prisma.user.findUnique({
+
+        });
+
+        try {
+
+        } catch (e) {
+          // handle the error lol
+          console.warn(e);
+          return null;
         }
-        // Return null if user data could not be retrieved
+
         return null;
       },
     }),
