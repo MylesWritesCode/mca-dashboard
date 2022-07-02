0fa673cc538dc1e9d1dc72db5e256ed59bad8522
MylesWritesCode <myles@themapletree.io>
2022-07-02 13:40:21 -1000

    upgrades packages

--- apps/web/repositories/OrganizationRepository.ts
+++ apps/web/repositories/OrganizationRepository.ts
@@ -1,5 +1,6 @@
 import prismaClient from "@/lib/prisma.client";
-import { logger } from "@/utils/logger";
+import { log } from "@/utils/logger";
 import { Organization, Prisma } from "@prisma/client";
 import { Session } from "next-auth";
 
+export {};
