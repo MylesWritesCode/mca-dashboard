c52589d2a53e21421aa7f96674f9f5221ae41286
MylesWritesCode <myles@themapletree.io>
2022-07-11 11:29:09 -1000

    adds progress bar example

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -11,6 +11,8 @@ const CHART_HEIGHT = 150;
 export default function Index() {
   const verticalBarChartRef = useRef<SVGSVGElement | null>(null);
   const horizontalBarChartRef = useRef<SVGSVGElement | null>(null);
+  const progressBarChartRef = useRef<HTMLDivElement | null>(null);
+
   const dataArray = [90, 80, 34, 75, 102, 68, 21, 8, 19, 104, 79];
 
   // Vertical bar graph
@@ -33,23 +35,61 @@ export default function Index() {
       .selectAll("rect")
       .data(dataArray)
       .enter()
+      .append("g")
       .append("rect")
-      .attr("x", (d, i) => BAR_HEIGHT)
       .attr("y", (d, i) => i * BAR_HEIGHT)
-      .attr("height", BAR_HEIGHT)
-      .attr("width", d => d)
+      .attr("height", BAR_HEIGHT - 2)
+      .attr("width", d => CHART_WIDTH - d * 2)
       .attr("stroke-width", "1px")
-      .attr("stroke", "#369")
-      .attr("fill", "#396");
+      .attr("fill", "#639");
+
+    const progress = d3
+      .select(progressBarChartRef.current)
+      .selectAll("svg")
+      .data(dataArray)
+      .enter()
+      .append("svg")
+      .attr("class", "rounded-md")
+      .attr("width", CHART_WIDTH)
+      .attr("height", BAR_HEIGHT);
+
+    progress
+      .append("rect")
+      .attr("class", "bg")
+      .attr("fill", "#00000033")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", CHART_WIDTH)
+      .attr("height", BAR_HEIGHT);
+
+    progress
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", d => CHART_WIDTH - d * 4)
+      .attr("height", BAR_HEIGHT)
+      .attr("fill", "#639");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="flex flex-col gap-4">
       <Card>
+        <Card.Header title="Progress chart with D3" />
+        <div className="flex justify-center align-center">
+          <div ref={progressBarChartRef} className="m-8 gap-2 flex flex-col rounded"></div>
+        </div>
+      </Card>
+      <Card>
         <Card.Header title="Horizontal bar charts with D3" />
         <div className="flex justify-center align-center">
           <div className="m-8">
-            <svg ref={horizontalBarChartRef} width={CHART_WIDTH} height={BAR_HEIGHT * dataArray.length - 1}></svg>
+            <svg
+              ref={horizontalBarChartRef}
+              width={CHART_WIDTH}
+              height={BAR_HEIGHT * dataArray.length - 1}
+              className=""></svg>
           </div>
         </div>
       </Card>
